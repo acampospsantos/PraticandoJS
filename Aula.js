440326a2257463bbeb2eeaee7a3303457b4d6e57
antsu comuna <acampospsantos@hotmail.com>
2022-05-11 23:55:09 -0300

    primeiro commit

---
+++ Aula.js
@@ -0,0 +1,43 @@
+//TEORIA
+
+// USANDO COMANDO DE IMPRIMIR
+//console.log('Hello, World!');
+//console.log('Testando!');
+
+// CRIANDO VARIÁVEL
+//let idade = 10;
+
+// CRIANDO VARIÁVEL E IMPRIMINDO SEU VALOR
+//let idade = 10;
+//console.log(idade);
+
+//
+console.log('Oiu - Classe Aula.js');
+
+//Tipos de Valores:
+//let nome = 'Lukita' --> string
+//let idade = 25 --> number
+//let aprovado = true --> boolean
+//let sobrenome = undefined => let sobrenome --> undefined
+//let corSelecionado = null --> null (usado para resetar um valor)
+
+//Tipos de Referência
+//Objeto
+//Arrays
+//Functions
+
+//Declarando um Objeto
+//let pessoa = {
+//    nome: 'Lauruta' ,
+//    sobrenome: 'Países Baixos' ,
+//    idade: 22 ,
+//    estaAprovada: true ,
+//    engenheiraRenomada: true
+//}
+//console.log(pessoa);
+
+//Criando Array
+//let array = ['oi', 20, true]; //Array pode armazenar vários tipos de valores(diferentes) 
+//console.log(array);
+//console.log(array[2]); //Imprime o elemento de indice 2 (nesse caso é o true)
+//console.log(array.length); //Imprime o tamanho do Array
---
+++ AulaExercicios.js
@@ -0,0 +1,10 @@
+//Primeiro Exercício: Imprimir três variáveis diferentes
+
+//Criando variáveis
+let idade = 10;
+let nome = "Anderson";
+let altura = 1.87;
+//Imprimindo variáveis
+console.log(idade);
+console.log(nome);
+console.log(altura);
